85601fb52b9916587d28e180e4650bc2de15fdbb
AlexaZhou <AlexaZhou@163.com>
2025-01-27 23:15:49 +0800

    restore to sutando orm

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import {Context, Hono, Next} from 'hono'
 import { chatCompletions } from './web/aiApiEntry'
-import { PrismaClient } from "@prisma/client";
-import { PrismaD1 } from "@prisma/adapter-d1";
 
-let ormClient:PrismaClient|null = null;
 
 declare module 'hono' {
   interface ContextVariableMap {
-    prisma: PrismaClient
   }
 }
 
@@ -18,14 +14,6 @@ export interface Env {
 const app = new Hono();
 
 async function prepareDBConnection(c:Context, next:Next){
-  if(ormClient == null){
-    console.log("prepareDBConnection");
-    const adapter = new PrismaD1(c.env.DB);
-    ormClient = new PrismaClient({ adapter });
-  }
-
-  c.set('prisma', ormClient);
-
   await next();
 }
 
@@ -36,12 +24,6 @@ app.get('/', (c) => {
 })
 
 app.get('/testORM.json', async (c) => {
-
-  const prisma = c.get('prisma');
-  const users = await prisma.user.findMany();
-
-  const result = JSON.stringify(users);
-  return new Response(result);
 })
 
 app.post('/v1/chat/completions', chatCompletions);
